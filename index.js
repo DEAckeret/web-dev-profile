60c0b1b451a066583cf1626f14b8a12977a4d231
D. E. Ackeret <101536271+DEAckeret@users.noreply.github.com>
2024-01-13 20:39:04 -0700

    made progress on structure/layout.

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 
+// $(document).ready(function() {
+//     $('.dark-mode-toggle').click(function() {
+
+//         if ($('.dark-mode-toggle').hasClass('dark')) {
+//             $('.dark-mode-toggle').removeClass('dark');
+//         } else {
+//             $('.dark-mode-toggle').addClass('dark');
+//         }
+        
+        
+//         if ($('.dark-mode-toggle').hasClass('dark')) {
+//             $('.moon').addClass('dark');
+//             $('.sun').addClass('dark');
+//             $('.header-name').addClass('dark');
+//             $('body').addClass('dark');
+//         } else {
+//             $('.moon').removeClass('dark');
+//             $('.sun').removeClass('dark');
+//             $('.header-name').removeClass('dark');
+//             $('body').removeClass('dark');
+//         }
+//     });
+// });
+
 $(document).ready(function() {
     $('.dark-mode-toggle').click(function() {
 
@@ -8,17 +32,23 @@ $(document).ready(function() {
             $('.dark-mode-toggle').addClass('dark');
         }
         
-        
         if ($('.dark-mode-toggle').hasClass('dark')) {
-            $('.moon').addClass('dark');
-            $('.sun').addClass('dark');
-            $('.header-name').addClass('dark');
+            $('body').find('.light').addClass('dark');
             $('body').addClass('dark');
-        } else {
-            $('.moon').removeClass('dark');
-            $('.sun').removeClass('dark');
-            $('.header-name').removeClass('dark');
+
+        } else { 
+            $('body').find('.light').removeClass('dark');
             $('body').removeClass('dark');
+            
         }
+
     });
 });
+
+
+// trying something out
+// $(document).ready(function() {
+//     $('.contact-me').click(function() {
+//         $("body").find(".old").removeClass("old").addClass("new");
+//     });
+// });
